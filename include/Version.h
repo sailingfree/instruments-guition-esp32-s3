2ef4383dcc552ed6ec1f740cf07f135490767b52
sailingfree <peter@naiadhmoe.com>
2026-08-19 22:13:32 +0100

    Add version increment

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.39"
+        #define VERSION "0.1.0"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-08-12 13:54:03.326446"
+        #define BUILD_TIMESTAMP "2025-09-21 21:59:37.397403"
     #endif
     
