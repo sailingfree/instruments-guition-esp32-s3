3e609a60d86051c641bce1b5ac5947f0fa8290dd
sailingfree <peter@naiadhmoe.com>
2026-09-12 15:24:49 +0100

    Use file data preserving fork of sdfat

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.81"
+        #define VERSION "0.1.107"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-09-11 09:41:55.404090"
+        #define BUILD_TIMESTAMP "2026-09-12 15:06:30.346990"
     #endif
     
